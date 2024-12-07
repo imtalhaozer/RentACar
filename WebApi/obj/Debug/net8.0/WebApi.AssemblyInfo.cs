5b7a4471c310a0ecc00506ef178c8d92bfca08e8
imtalhaozer <imtalhaozer@gmail.com>
2024-12-07 14:14:15 +0300

    some changes

--- WebApi/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
+++ WebApi/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3593e5be97e76694975449937568e1f2d7eeb9d9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1833063112108c4b1833e61b92e1798a2811da8a")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
